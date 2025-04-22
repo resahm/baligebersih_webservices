c988ea981d519b14065eb79debca99ff5807ccb4
Resa Manurung <124247123+resahm@users.noreply.github.com>
2025-04-22 14:07:17 +0700

    Update firebaseService.js

--- services/firebaseService.js
+++ services/firebaseService.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 
 // Ambil file serviceAccountKey dari Firebase Console
-const serviceAccount = require('../config/firebase-admin-sdk.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
